bfb0550fec468568acf49257be982b75ebde1352
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-02-22 09:50:00 -0300

    Create instalar-tmux.sh

---
+++ instalar-tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update && apt install tmux -y
+
+echo `
+SESSIONNAME="script"
+tmux has-session -t $SESSIONNAME &> /dev/null
+if [ $? != 0 ]
+  then
+    tmux new-session -s $SESSIONNAME -n script -d
+fi
+tmux attach -t $SESSIONNAME
+` >> /root/.bashrc
