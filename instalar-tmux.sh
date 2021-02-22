ab04e656697790e048839357bdefa6485749d432
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2021-02-22 10:27:21 -0300

    Update instalar-tmux.sh

--- instalar-tmux.sh
+++ instalar-tmux.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 apt update && apt install tmux -y
 
-tee -a /root/.bashrc <<<EOT
+tee -a /root/.bashrc << END
 tmux has-session -t SESSAOPAI &> /dev/null
-if [ $? != 0 ]
+if [ \$? != 0 ]
   then
     tmux new-session -s SESSAOPAI -n script -d
 fi
 tmux attach -t SESSAOPAI
-EOT;
+END
 
 # bash <(curl -s https://raw.githubusercontent.com/wzuqui/wzuqui/main/instalar-tmux.sh)
