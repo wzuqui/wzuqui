dd7049965a7ab56209bd90130167cb960993f07f
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2022-09-21 10:43:15 -0300

    Update XmlTransform.Install.ps1

--- XmlTransform.Install.ps1
+++ XmlTransform.Install.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest https://github.com/wzuqui/wzuqui/raw/main/XmlTransform.ps1 XmlTransform.ps1
-Invoke-WebRequest https://github.com/wzuqui/wzuqui/raw/main/Microsoft.Web.XmlTransform.dll Microsoft.Web.XmlTransform.dll
+Invoke-WebRequest 'https://github.com/wzuqui/wzuqui/raw/main/XmlTransform.ps1' -OutFile 'XmlTransform.ps1'
+Invoke-WebRequest 'https://github.com/wzuqui/wzuqui/raw/main/Microsoft.Web.XmlTransform.dll' -OutFile 'Microsoft.Web.XmlTransform.dll'
