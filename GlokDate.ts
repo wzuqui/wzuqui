5c1ab9fea6d6be82958e7bc94e1a92d63429cfed
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2022-10-25 09:03:11 -0300

    Update GlokDate.ts

--- GlokDate.ts
+++ GlokDate.ts
@@ -1,20 +1,29 @@
 class GlokDate extends Date {
-  adicionarMes() {
-    this.setMonth(this.getMonth()+1);
-  }
-  adicionarDia() {
-    this.setDate(this.getDate()+1);
-  }
-  diminiurDia() {
-    this.setDate(this.getDate()-1);
-  }
-  primeiroDiaDoMes() {
-    return new Date(this.setDate(1));
-  }
-  ultimoDiaDoMes() {
-    const retorno = new GlokDate(this);
-    retorno.adicionarMes();
-    retorno.diminiurDia();
-    return retorno;
-  }
+  adicionarMes() {
+    this.setMonth(this.getMonth()+1);
+  }
+  adicionarDia() {
+    this.setDate(this.getDate()+1);
+  }
+  diminiurDia() {
+    this.setDate(this.getDate()-1);
+  }
+  primeiroDiaDoMes() {
+    return new Date(this.setDate(1));
+  }
+  ultimoDiaDoMes() {
+    const retorno = new GlokDate(this);
+    retorno.primeiroDiaDoMes();
+    retorno.adicionarMes();
+    retorno.diminiurDia();
+    return retorno;
+  }
+  toString = () : string => {
+    return `${this.toISOString()}`;
+  }
 }
+
+console.clear();
+const a = new GlokDate();
+console.log(`${a.primeiroDiaDoMes().toISOString()}`)
+console.log(`${a.ultimoDiaDoMes()}`)
